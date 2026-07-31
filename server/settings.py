168e5da3433fadda97b88dd7763a6a434d17ff95
ananya <ananyaravikumar71@gmail.com>
2026-07-31 12:11:58 +0530

    Backend Version 1: Django setup with custom user model and admin

--- server/settings.py
+++ server/settings.py
@@ -37,9 +37,16 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
     'rest_framework',
     'rest_framework_simplejwt',
     'corsheaders',
+
+    'accounts',
+    'tickets',
+    'responses',
+    'core',
+    'nlp_engine',
 ]
 
 MIDDLEWARE = [
